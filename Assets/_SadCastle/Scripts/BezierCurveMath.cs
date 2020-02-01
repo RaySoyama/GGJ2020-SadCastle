314f3b8e3187029e942612a38d0925cc690191a8
diammen <marcelnamdao@gmail.com>
2020-01-31 22:35:19 -0800

    import toon shader

---
+++ Assets/_SadCastle/Scripts/BezierCurveMath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Bezier
+{
+    public static Vector3 QuadraticBezier(Vector3 a, Vector3 b, Vector3 c, float t)
+    {
+        Vector3 x = Vector3.Lerp(a, b, t);
+        Vector3 y = Vector3.Lerp(b, c, t);
+
+        return Vector3.Lerp(x, y, t);
+    }
+}
---
+++ Assets/_SadCastle/Scripts/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public Vector3 target;
+    public bool move;
+
+    Vector3 start, mid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = transform.position;
+        mid = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (move)
+        {
+            Bezier.QuadraticBezier(start, mid, target, Time.deltaTime);
+        }
+    }
+}
