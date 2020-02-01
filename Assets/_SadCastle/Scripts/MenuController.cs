8b8b059280c7c175289f8168915bd0d799c7b846
Terry Nguyen <terreh@terrehbyte.com>
2020-02-01 00:59:33 -0800

    Add DOTween and fancy UI

---
+++ Assets/_SadCastle/Scripts/MenuController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+
+public class MenuController : MonoBehaviour
+{
+    public UnityEventBool OnMenuVisibilityChanged;
+    public UnityEventGameObject OnMenuStateChange;
+
+    public bool showOnStart = false;
+
+    [System.Serializable]
+    public class ElementStartStop
+    {
+        public RectTransform elementRect;
+        public Vector2 startPosition;
+        public Vector2 stopPosition;
+        public float duration;
+    }
+    public ElementStartStop[] elements;
+
+    public void Start()
+    {
+        if(showOnStart)
+        {
+            ShowMenu();
+        }
+    }
+
+    [ContextMenu("Show Menu")]
+    public void ShowMenu()
+    {
+        OnMenuVisibilityChanged?.Invoke(true);
+
+        foreach (var elem in elements)
+        {
+            elem.elementRect.DOAnchorPos(elem.stopPosition, elem.duration, false);
+        }
+    }
+
+    [ContextMenu("Hide Menu")]
+    public void HideMenu()
+    {
+        OnMenuVisibilityChanged?.Invoke(false);
+
+        foreach (var elem in elements)
+        {
+            elem.elementRect.DOAnchorPos(elem.startPosition, elem.duration, false).SetEase(Ease.InBounce);
+        }
+    }
+}
+
+[System.Serializable]
+public class UnityEventGameObject : UnityEvent<GameObject> { }
+[System.Serializable]
+public class UnityEventBool : UnityEvent<bool> { }
