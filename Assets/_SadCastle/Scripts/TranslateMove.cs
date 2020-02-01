1bd1e94572bd6a858d16f5646a4eda68aba07cae
diammen <marcelnamdao@gmail.com>
2020-02-01 11:23:31 -0800

    2 move types

---
+++ Assets/_SadCastle/Scripts/TranslateMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateMove : Entity
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Move()
+    {
+        if (move)
+        {
+            transform.position = Vector3.Lerp(start, target.position, Time.deltaTime);
+        }
+    }
+}
