6303a173b36facced5e36f67367eca0ddd1512e3
Ray Soyama <raysoyama@gmail.com>
2020-02-01 12:09:51 -0800

    Created Template for World Machine Spawner.

---
+++ Assets/_SadCastle/Scripts/WorldMachine.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMachine : MonoBehaviour
+{
+    [System.Serializable]
+    struct SpawnTimerObject
+    {
+        public float InGameTime;
+        //enum      
+    }
+
+    [SerializeField]
+    private List<SpawnTimerObject> SpawnTimeline = new List<SpawnTimerObject>();
+
+
+    [ReadOnlyField] [SerializeField]
+    private float time = 0;
+
+    
+
+
+    void Start()
+    {
+   
+
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (SpawnTimeline.Count != 0 && time >= SpawnTimeline[0].InGameTime)
+        {
+            //Call Marcel script
+            Debug.Log("Spawned Object");
+            SpawnTimeline.RemoveAt(0);
+        }
+
+
+    }
+}
