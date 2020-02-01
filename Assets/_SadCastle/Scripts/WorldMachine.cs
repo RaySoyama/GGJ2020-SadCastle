687ed8ced58285ebb5ec860daf866a560b880057
diammen <marcelnamdao@gmail.com>
2020-02-01 13:15:12 -0800

    Update WorldMachine.cs

--- Assets/_SadCastle/Scripts/WorldMachine.cs
+++ Assets/_SadCastle/Scripts/WorldMachine.cs
@@ -25,7 +25,7 @@ public class WorldMachine : MonoBehaviour
 
     void Start()
     {
-        spawnManager = SpawnManager.SuperSpawnManager;
+        spawnManager = SpawnManager.instance;
     
         
     }
