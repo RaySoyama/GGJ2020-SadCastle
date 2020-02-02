aa7ac801b9db45fe98a755ee1aa6f66178b03e40
shanencross <shanencross@yahoo.com>
2020-02-02 12:58:23 -0800

    rearranged Castle prefab and modified script so that script is on parent objects of models

--- Assets/_SadCastle/Scripts/CastleChunk.cs
+++ Assets/_SadCastle/Scripts/CastleChunk.cs
@@ -17,8 +17,8 @@ public class CastleChunk : MonoBehaviour
 
     void Awake()
     {
-        meshRenderer = GetComponent<MeshRenderer>();
-        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+        meshFilter = GetComponentInChildren<MeshFilter>();
     }
 
     void OnMouseOver()
