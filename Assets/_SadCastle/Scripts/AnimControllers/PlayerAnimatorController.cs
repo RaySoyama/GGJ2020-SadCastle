231a28a748b5899c947ee70e3c2ee33a648dc3f4
Terry Nguyen <terreh@terrehbyte.com>
2020-02-02 11:46:22 -0800

    Animate Player character when run/fix

---
+++ Assets/_SadCastle/Scripts/AnimControllers/PlayerAnimatorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimatorController : MonoBehaviour
+{
+    public Animator animator;
+    public NavMeshAgent agent;
+
+    public int numberOfRepairAnimations = 2;
+
+    public void HandleChunkRepair(CastleChunk repairedChunk)
+    {
+        animator.SetTrigger("DoRepair");
+        animator.SetInteger("NextRepairType", Random.Range(0, numberOfRepairAnimations));
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+
+    void Reset()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+}
