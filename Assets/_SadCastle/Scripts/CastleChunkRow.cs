f2b6f72ae7884dd7a48ebe92adbc2c4eb402cf00
shanencross <shanencross@yahoo.com>
2020-02-01 17:56:06 -0800

    skeleton for Castle script containing effective 2D array of chunks

---
+++ Assets/_SadCastle/Scripts/CastleChunkRow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CastleChunkRow 
+{
+    [SerializeField] CastleChunk[] castleChunks;
+
+    public CastleChunk[] CastleChunks {
+        get {
+            return castleChunks;
+        }
+    }
+}
